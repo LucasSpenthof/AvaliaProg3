a2439a7b649d5746986568a59ed687fbdfadfbfa
LucasSpenthof <lucas.spenthof@gmail.com>
2021-06-13 13:47:13 -0400

    Configuração de variáveis de ambiente #10

--- app/config/database.js
+++ app/config/database.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     dialect:'postgres', // define o  SGDB a ser utilizado
     host:'localhost',
     username:'postgres', // nome de usuário padrão no postgres
@@ -6,7 +6,19 @@ module.exports = {
     database:'nps',
     define:{
         timestamps:true,
-				/* define a nomenclatura com separação de palavaras por (_) underline*/
+				
+				underscored: true, 
+    }
+}*/
+
+module.exports = {
+    dialect:process.env.DB_DIALECT, 
+    host:process.env.DB_HOST,
+    username:process.env.DB_USER, 
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    define:{
+        timestamps:true,
 				underscored: true, 
     }
 }
